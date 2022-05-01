93d2312dcddc5b94fc538636afafb751d8ba534e
Philipp Derksen <derksenphilipp@gmail.com>
2022-05-01 22:29:22 +0200

    Header and navigation.

---
+++ src/components/appHeader.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import TopAppBar, {
+    Row,
+    Section,
+    Title
+  } from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+  import { createEventDispatcher } from 'svelte';
+
+  export let modal : Boolean = false;
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<TopAppBar variant="standard" class={modal ? "my-bar" : "my-bar top"}>
+  <Row>
+    <Section>
+      {#if modal}
+        <IconButton class="material-icons" on:click={() => {dispatch('menuClick')}}>menu</IconButton>
+      {/if}
+      <Title>Hanna</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Download"
+        >file_download</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Print this page"
+        >print</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Bookmark this page"
+        >bookmark</IconButton
+      >
+    </Section>
+  </Row>
+</TopAppBar>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,97 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import {Label} from '@smui/common';
+  import Button from '@smui/button';
+  import {AppContent} from '@smui/drawer';
+  import Card from '@smui/card';
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
+  import Accordion, { Panel, Header as PanelHeader, Content as PanelContent } from '@smui-extra/accordion';
+
+  import AppHeader from '../components/appHeader.svelte';
+  import AppNav from '../components/appNav.svelte';
+  import MediaQuery from '../components/MediaQuery.svelte';
+
+  let navOpen = false;
+  
+  let lw : number;
+  let cw : number;
+  let ww : number;
+</script>
+<MediaQuery query="(max-width: 599px)" let:matches>
+  <AppHeader modal={matches} on:menuClick={() => navOpen = !navOpen}/>
+  <AppNav modal={matches} open={navOpen}/>
+</MediaQuery>
+<AppContent class="mdc-top-app-bar--fixed-adjust">
+  <LayoutGrid>
+    <Cell spanDevices={{ desktop: 6, tablet: 8, phone: 4 }}>
+      <div bind:clientWidth={cw}>
+        <Card padded>
+          <p>Card 1</p>
+          <p>Window width: {ww}</p>
+          <p>Layout grid width: {lw}</p>
+          <p>Card width: {cw}</p>
+        </Card>
+      </div>
+      <div class="card-spacer"/>
+      <Card padded>Card 2</Card>
+      <div class="card-spacer"/>
+      <Accordion>
+        <Panel>
+          <PanelHeader>Panel 1</PanelHeader>
+          <PanelContent>
+            The content for panel 1.
+            <ul>
+              <li>Some</li>
+              <li>List</li>
+              <li>Items</li>
+            </ul>
+          </PanelContent>
+        </Panel>
+      </Accordion>
+    </Cell>
+    <Cell spanDevices={{ desktop: 6, tablet: 8, phone: 4 }}>
+      <Card padded>Card 3</Card>
+      <div class="card-spacer"/>
+      <Card padded>Card 4</Card>
+      <div class="card-spacer"/>
+      <Card padded>Card 5</Card>
+    </Cell>
+    <Cell spanDevices={{ desktop: 12, tablet: 8, phone: 4 }}>
+      <div class="action-row">
+        <div class="apply-button">
+          <Button variant="raised">
+            <Label>Apply</Label>
+          </Button>
+        </div>
+        <Button variant="raised">
+          <Label>Reload</Label>
+        </Button>
+      </div>
+    </Cell>
+  </LayoutGrid>
+</AppContent>
+
+<style>
+  :global(body) {
+    width: 100vw;
+    margin: 0px;
+    display: flex;
+  }
+
+  :global(.mdc-drawer-app-content) {
+    width: 100%;
+  }
+
+  .card-spacer {
+    margin-bottom: 15px;
+  }
+
+  .action-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+
+  .apply-button {
+    margin-right: 15px;
+  }
+</style>
